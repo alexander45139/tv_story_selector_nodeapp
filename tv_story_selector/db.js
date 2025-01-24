62ed0d50ced65db92b9210900218e71bff46f3e4
alexander45139 <61944904+alexander45139@users.noreply.github.com>
2025-01-24 18:08:44 +0000

    New DB connection to local db

--- tv_story_selector/db.js
+++ tv_story_selector/db.js
@@ -1,23 +1,40 @@
-const mysql = require('mysql2/promise');
-
-const credentials = {
-	/*host: 'am2012.brighton.domains',
-	user: 'am2012_main',
-	password: 'Fer33doubleH?',
-	database: 'am2012_tv_story_selector'*/
-
-	host: '127.0.0.1',
-	user: 'root',
-	password: '',
-	database: 'tv_story_selector'
-};
+const odbc = require('odbc');
 
 async function query(sql, params) {
-	const connection = await mysql.createConnection(credentials);
-	const [results, ] = await connection.execute(sql, params);
-	return results;
+    const connectionString = 'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=../TV Story Selector.accdb';
+
+    try {
+        const connection = await odbc.connect(connectionString);
+        const results = await connection.query(sql, params);
+        await connection.close();
+    } catch (err) {
+        console.error('Error:', err);
+    }
 }
 
-module.exports = {
-	query
-}
+
+
+
+//const mysql = require('mysql2/promise');
+
+//const credentials = {
+//	/*host: 'am2012.brighton.domains',
+//	user: 'am2012_main',
+//	password: 'Fer33doubleH?',
+//	database: 'am2012_tv_story_selector'*/
+
+//	host: '127.0.0.1',
+//	user: 'root',
+//	password: '',
+//	database: 'tv_story_selector'
+//};
+
+//async function query(sql, params) {
+//	const connection = await mysql.createConnection(credentials);
+//	const [results, ] = await connection.execute(sql, params);
+//	return results;
+//}
+
+//module.exports = {
+//	query
+//}
