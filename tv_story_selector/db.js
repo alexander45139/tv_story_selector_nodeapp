ccd3d488cf1208c6a63837b665c3ea5bd8117a82
alexander45139 <61944904+alexander45139@users.noreply.github.com>
2025-01-30 17:53:40 +0000

    PostgreSQL db credentials

--- tv_story_selector/db.js
+++ tv_story_selector/db.js
@@ -1,40 +1,35 @@
-const odbc = require('odbc');
+/* POSTGRE SQL */
+const { Pool } = require('pg');
 
-async function query(sql, params) {
-    const connectionString = 'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=../TV Story Selector.accdb';
-
-    try {
-        const connection = await odbc.connect(connectionString);
-        const results = await connection.query(sql, params);
-        await connection.close();
-    } catch (err) {
-        console.error('Error:', err);
-    }
-}
+const pool = new Pool({
+    host: 'localhost',
+    database: 'tv_story_selector',
+    user: 'tv_story_user',
+    pssword: 'user_123'
+});
 
 
+/* MYSQL */
+// const mysql = require('mysql2/promise');
 
+// const credentials = {
+// 	/*host: 'am2012.brighton.domains',
+// 	user: 'am2012_main',
+// 	password: 'Fer33doubleH?',
+// 	database: 'am2012_tv_story_selector'*/
 
-//const mysql = require('mysql2/promise');
+// 	host: '127.0.0.1',
+// 	user: 'root',
+// 	password: '',
+// 	database: 'tv_story_selector'
+// };
 
-//const credentials = {
-//	/*host: 'am2012.brighton.domains',
-//	user: 'am2012_main',
-//	password: 'Fer33doubleH?',
-//	database: 'am2012_tv_story_selector'*/
+// async function query(sql, params) {
+// 	const connection = await mysql.createConnection(credentials);
+// 	const [results, ] = await connection.execute(sql, params);
+// 	return results;
+// }
 
-//	host: '127.0.0.1',
-//	user: 'root',
-//	password: '',
-//	database: 'tv_story_selector'
-//};
-
-//async function query(sql, params) {
-//	const connection = await mysql.createConnection(credentials);
-//	const [results, ] = await connection.execute(sql, params);
-//	return results;
-//}
-
-//module.exports = {
-//	query
-//}
+// module.exports = {
+// 	query
+// }
