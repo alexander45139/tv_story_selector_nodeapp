7b137b6d9f59b22efff0e7a230baf9354ec9d5af
alexander45139 <61944904+alexander45139@users.noreply.github.com>
2025-02-28 18:02:27 +0000

    Update 28/02/2025 18:02

--- tv_story_selector/db.js
+++ tv_story_selector/db.js
@@ -1,5 +1,5 @@
 /* POSTGRE SQL */
-const { Pool } = require('pg');
+/* const { Pool } = require('pg');
 
 const pool = new Pool({
     host: 'localhost',
@@ -10,29 +10,29 @@ const pool = new Pool({
 });
 
 module.exports = pool;
-
+ */
 
 /* MYSQL */
-// const mysql = require('mysql2/promise');
+const mysql = require('mysql2/promise');
 
-// const credentials = {
-// 	/*host: 'am2012.brighton.domains',
-// 	user: 'am2012_main',
-// 	password: 'Fer33doubleH?',
-// 	database: 'am2012_tv_story_selector'*/
+const credentials = {
+	/*host: 'am2012.brighton.domains',
+	user: 'am2012_main',
+	password: 'Fer33doubleH?',
+	database: 'am2012_tv_story_selector'*/
 
-// 	host: '127.0.0.1',
-// 	user: 'root',
-// 	password: '',
-// 	database: 'tv_story_selector'
-// };
+	host: '127.0.0.1',
+	user: 'root',
+	password: '',
+	database: 'tv_story_selector'
+};
 
-// async function query(sql, params) {
-// 	const connection = await mysql.createConnection(credentials);
-// 	const [results, ] = await connection.execute(sql, params);
-// 	return results;
-// }
+async function query(sql, params) {
+	const connection = await mysql.createConnection(credentials);
+	const [results, ] = await connection.execute(sql, params);
+	return results;
+}
 
-// module.exports = {
-// 	query
-// }
+module.exports = {
+	query
+}
